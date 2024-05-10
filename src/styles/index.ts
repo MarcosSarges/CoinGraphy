9a168e3acf78a67f236c4526e8e7b4ee1c1b875f
Marcos Sarges <marcos.sarges@aurum.com.br>
2024-05-10 16:02:19 -0300

    style: configuracao do tema

---
+++ src/styles/index.ts
@@ -0,0 +1,13 @@
+import fonts from "./fonts";
+import colors from "./colors";
+import spacings from "./spacings";
+import sizes from "./sizes";
+
+export type TypeTheme = {
+  fonts: typeof fonts;
+  colors: typeof colors;
+  spacings: typeof spacings;
+  sizes: typeof sizes;
+};
+
+export const theme: TypeTheme = { fonts, colors, spacings, sizes };
